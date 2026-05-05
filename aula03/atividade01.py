623801367e6a769c1ca430a80c3e0f27afa3bd40
higorcamelo11 <higorcamelo11@gmail.com>
2026-05-04 21:25:48 -0300

    conclusão da atividade01

---
+++ aula03/atividade01.py
@@ -0,0 +1,8 @@
+valor = int(input('\nValor da compra: '))
+
+if valor >= 250:
+    desc = valor * 0.16
+    total = valor - desc
+    print(f'\nTotal com Desconto: {total}')
+else:
+    print(f'\nTotal: {valor}')
